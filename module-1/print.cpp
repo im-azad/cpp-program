7258520757461bb348ce31b82db96c962f280f00
im-azad <me.azad99@gmail.com>
2023-05-21 07:01:16 +0600

    c++ program print and run

---
+++ module-1/print.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// #include <stdio.h>
+int main()
+{
+    std::cout << "hello world \n";
+    std::cout << "I love bangladesh \n";
+    std::cout << "Hello \n"
+              << "C++ program";
+    return 0;
+}
